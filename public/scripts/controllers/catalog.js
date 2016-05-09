1974dd73f74d346ceca97905173d1a15671bb683
Eric Fulkerson <eric.fulkerson@us.sogeti.com>
2016-05-09 15:05:15 +0100

    upgrade karma and lint cleaning, as well as changes to the path

--- public/scripts/controllers/catalog.js
+++ public/scripts/controllers/catalog.js
@@ -2,12 +2,12 @@
 
 define(['angular', '../modules/sample-module/sample-module'], function(angular, controllers){
 	controllers.controller('CatalogCtrl', ['$http','$scope', function($http,$scope){
-		$http.get("https://predix-analytics-catalog-release.run.aws-usw02-pr.ice.predix.io/api/v1/catalog/analytics")
+		$http.get('api/predix-analytics-catalog-new-ui/catalog/analytics')
 			.then(function(response){
 				$scope.catalog = response.data;
 			},
 			function(failureCallback){
-				console.log("You are " + failureCallback.statusText)
+				console.log('You are ' + failureCallback.statusText);
 			});
 	}]);
 });
