e93ff939a0b692c24607ea43b504c4dfaab1f757
Eric Fulkerson <eric.fulkerson@us.sogeti.com>
2016-05-11 16:43:06 +0100

    adding new dummy data and reorganizing the px-table data

--- public/scripts/controllers/catalog.js
+++ public/scripts/controllers/catalog.js
@@ -4,7 +4,28 @@ define(['angular', '../modules/sample-module/sample-module'], function(angular, 
 	controllers.controller('CatalogCtrl', ['$scope', function($scope){
 		$scope.catalog = [
 		    {
-		    	
+		    	taxomonyLocation: "/predix-demo-analytics",
+		    	name: "anomalyDetector-py",
+		    	author: "DataScience+PredixAnalytics",
+		    	description: "Does something with data points",
+		    	version: "v1",
+		    	language:"Python"
+		    },
+		    {
+		    	taxomonyLocation: "/predix-demo-analytics",
+		    	name: "demoAdder",
+		    	author: "predix-analytics",
+		    	description: "given 2 long numbers it does something",
+		    	version: "v1",
+		    	language:"Python"
+		    },
+		    {
+		    	taxomonyLocation: "/predix-demo-analytics",
+		    	name: "demoAdder-py",
+		    	author: "predix-analytics",
+		    	description: "given 2 long numbers it does something else",
+		    	version: "v1",
+		    	language:"Python"
 		    }
 		];
 	}]);
