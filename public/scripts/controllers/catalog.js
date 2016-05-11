b6e3dc77554cc8ba3a22be6e22ec5e7f008efc03
Eric Fulkerson <eric.fulkerson@us.sogeti.com>
2016-05-11 16:19:20 +0100

    restructured the modal tables and removed the dummy data from the catalog table

--- public/scripts/controllers/catalog.js
+++ public/scripts/controllers/catalog.js
@@ -4,14 +4,7 @@ define(['angular', '../modules/sample-module/sample-module'], function(angular, 
 	controllers.controller('CatalogCtrl', ['$scope', function($scope){
 		$scope.catalog = [
 		    {
-		    	id: 1,
-		    	name: "Hello World",
-		    	value: "This is my value"
-		    },
-		    {
-		    	id: 2,
-		    	name: "World Hello",
-		    	value: "This is my other value"
+		    	
 		    }
 		];
 	}]);
